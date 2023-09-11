a4c85e3a785abe237ba80579a7d54dfd48a6ebf6
RodiaGiordano <rodiagiordano93@gmail.com>
2023-09-11 17:25:11 +0200

    feat: add card

---
+++ vite-project/src/components/appCard.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  props: { card: Object },
+};
+</script>
+
+<template>
+  <div class="card">
+    <img :src="card.thumb" alt="" />
+    <span>{{ card.series }}</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  width: calc(100% / 6 - 20px);
+  font-size: 0.6rem;
+
+  img {
+    width: 100%;
+    aspect-ratio: 1;
+    object-fit: cover;
+    object-position: 0% 0%;
+  }
+}
+</style>
--- vite-project/src/components/appMain.vue
+++ vite-project/src/components/appMain.vue
@@ -1,7 +1,95 @@
 <script>
+import appCard from "./appCard.vue";
+
 export default {
+  components: { appCard },
   data() {
-    return {};
+    return {
+      dcComicsPic: [
+        {
+          thumb: "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+          price: "$19.99",
+          series: "Action Comics",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+          price: "$3.99",
+          series: "American Vampire 1976",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+          price: "$16.99",
+          series: "Aquaman",
+          type: "graphic novel",
+        },
+        {
+          thumb: "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+          price: "$2.99",
+          series: "Batgirl",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+          price: "$3.99",
+          series: "Batman",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+          price: "$2.99",
+          series: "Batman Beyond",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+          price: "$3.99",
+          series: "Batman/Superman",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+          price: "$4.99",
+          series: "Batman/Superman Annual",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+          price: "$5.99",
+          series: "Batman: The Joker War Zone",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+          price: "$6.99",
+          series: "Batman: Three Jokers",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+          price: "$4.99",
+          series: "Batman: White Knight Presents: Harley Quinn",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+          price: "$16.99",
+          series: "Catwoman",
+          type: "graphic novel",
+        },
+      ],
+    };
   },
 };
 </script>
@@ -11,12 +99,16 @@ export default {
     <div class="jumbo">
       <img src="../../public/vue-dc-comics-1/img/jumbotron.jpg" alt="" />
     </div>
-    <div class="main_wrapper"></div>
+    <div class="main_wrapper">
+      <appCard v-for="card in dcComicsPic" :card="card" />
+      <div class="load_more">LOAD MORE</div>
+    </div>
   </main>
 </template>
 
 <style lang="scss" scoped>
 main {
+  background-color: #1c1c1c;
   .jumbo {
     height: 400px;
 
@@ -24,20 +116,29 @@ main {
       height: 100%;
       width: 100%;
       object-fit: cover;
-      object-position: 0% 0%;
+      object-position: 0% -40%;
     }
   }
   .main_wrapper {
-    padding: 50px 0;
+    padding: 50px 0 20px;
 
     width: 75%;
     margin: auto;
     color: white;
 
     display: flex;
-    align-items: center;
-
+    flex-wrap: wrap;
+    gap: 20px;
     font-size: 1.5rem;
+  }
+  .load_more {
+    font-size: 0.8rem;
+    font-weight: bold;
+
+    padding: 10px 40px;
+    margin: 0 auto;
+    background-color: #0282f9;
+    color: white;
   }
 }
 </style>
